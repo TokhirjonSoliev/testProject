be42d0ebb43f9b386827248f881bf34400343d46
Tokhirjon <tokhirjon007@gmail.com>
2023-03-25 15:49:33 +0100

    minor changes

--- src/main/java/com/notes/notesservice/controller/ContentController.java
+++ src/main/java/com/notes/notesservice/controller/ContentController.java
@@ -15,36 +15,64 @@ import java.util.List;
 public class ContentController {
     private final ContentService contentService;
 
+    /**
+     * This is an api which is used to get all Contents
+     * @return List<Content>
+     */
     @GetMapping(value = "/all")
     public ResponseEntity<List<Content>> getAllNotes() {
         return ResponseEntity.ok(contentService.getAllContents());
     }
 
+    /**
+     * This is an api which is used to get all Contents in order from new to old
+     * @return List<Content>
+     */
     @GetMapping(value = "/order")
     public ResponseEntity<List<Content>> getAllOrderedNotes() {
         return ResponseEntity.ok(contentService.getAllOrderedContents());
     }
 
+    /**
+     * This is an api which is used to create a Content
+     * @return Content
+     */
     @PostMapping(value = "/create")
     public ResponseEntity<Content> createNotes(@RequestBody ContentDto contentDto) {
         return ResponseEntity.ok(contentService.createContent(contentDto));
     }
 
+    /**
+     * This is an api which is used to update a Content
+     * @return Content
+     */
     @PutMapping(value = "/edit/{content_id}")
     public ResponseEntity<Content> editNotes(@PathVariable("content_id") String contentId, @RequestBody ContentDto contentDto) {
         return ResponseEntity.ok(contentService.updateContent(contentId, contentDto));
     }
 
+    /**
+     * This is an api which is used to delete a Content
+     * @return String
+     */
     @DeleteMapping(value = "/delete/{content_id}")
     public ResponseEntity<String> deleteNotes(@PathVariable("content_id") String contentId) {
         return ResponseEntity.ok(contentService.deleteContent(contentId));
     }
 
+    /**
+     * This is an api which is used to give a like to Content
+     * @return String
+     */
     @PutMapping(value = "/like/{content_id}")
     public ResponseEntity<String> likeNotes(@PathVariable("content_id") String contentId) {
         return ResponseEntity.ok(contentService.likeContent(contentId));
     }
 
+    /**
+     * This is an api which is used to give a dislike to Content
+     * @return String
+     */
     @PutMapping(value = "/dislike/{content_id}")
     public ResponseEntity<String> dislikeNotes(@PathVariable("content_id") String contentId) {
         return ResponseEntity.ok(contentService.dislikeContent(contentId));
